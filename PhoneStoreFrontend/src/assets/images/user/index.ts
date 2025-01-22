68f6710ec4a011879bed7d8c6bae558cd43deb06
BienVan2003 <ngoisao2310@gmail.com>
2025-01-22 17:09:49 +0700

    build adminlayout

---
+++ PhoneStoreFrontend/src/assets/images/user/index.ts
@@ -0,0 +1,5 @@
+export { default as UserOne} from './user-01.png'
+export { default as UserTwo} from './user-02.png'
+export { default as UserThree} from './user-03.png'
+export { default as UserFour} from './user-04.png'
+
--- PhoneStoreFrontend/src/main.tsx
+++ PhoneStoreFrontend/src/main.tsx
@@ -1,15 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { persistor, store } from './store.ts'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css'
-import App from './App.tsx'
-import 'swiper/swiper-bundle.css'
 import { PersistGate } from 'redux-persist/integration/react'
+import 'swiper/swiper-bundle.css'
+import App from './App.tsx'
 import { AppLoading } from './components/index.ts'
+import './index.css'
+import { persistor, store } from './store.ts'
 
 const queryClient = new QueryClient()
 
@@ -18,7 +19,9 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <PersistGate loading={<AppLoading />} persistor={persistor}>
-          <App />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
         </PersistGate>
       </Provider>
       <ReactQueryDevtools initialIsOpen={false} />
